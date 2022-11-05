ca244b763b99d95fdb145f5f04a673b5ab9f7c0b
Cation-Coder6 <200516@kiit.ac.in>
2022-11-06 02:00:18 +0530

    added hostel specific view

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,35 +1,35 @@
 const express = require('express')
 const router = express.Router()
 const {
-  authenticateUser,
-  authorizePermissions,
+    authenticateUser,
+    authorizePermissions,
 } = require('../middleware/authentication')
 
 const {
-  createProduct,
-  getAllProducts,
-  getSingleProduct,
-  updateProduct,
-  deleteProduct,
-  uploadImage,
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadImage,
 } = require('../controllers/productController')
 
 const { getSingleProductReviews } = require('../controllers/reviewController')
 
 router
-  .route('/')
-  .post([authenticateUser], createProduct)
-  .get(getAllProducts)
+    .route('/')
+    .post([authenticateUser], createProduct)
+    .get([authenticateUser], getAllProducts)
 
 router
-  .route('/uploadImage')
-  .post([authenticateUser], uploadImage)
+    .route('/uploadImage')
+    .post([authenticateUser], uploadImage)
 
 router
-  .route('/:id')
-  .get(getSingleProduct)
-  .patch([authenticateUser], updateProduct)
-  .delete([authenticateUser], deleteProduct)
+    .route('/:id')
+    .get(getSingleProduct)
+    .patch([authenticateUser], updateProduct)
+    .delete([authenticateUser], deleteProduct)
 
 router.route('/:id/reviews').get(getSingleProductReviews)
 
