0ecb6595c6885b9add326e44d1364510f5a82bea
Cation-Coder6 <200516@kiit.ac.in>
2022-11-06 00:50:14 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 require("dotenv").config()
 require("express-async-errors")
-// express
+    // express
 
 const express = require("express")
 const app = express()
-// rest of the packages
+    // rest of the packages
 const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const fileUpload = require("express-fileupload")
@@ -30,10 +30,10 @@ const errorHandlerMiddleware = require("./middleware/error-handler")
 
 app.set("trust proxy", 1)
 app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 60,
-  })
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 60,
+    })
 )
 app.use(helmet())
 app.use(cors())
@@ -56,15 +56,15 @@ app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI)
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    )
-  } catch (error) {
-    console.log(error)
-  }
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        )
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 start()
