8e7131842278f48175c5338d3ff784fc6ee8cd8d
Cation-Coder6 <200516@kiit.ac.in>
2022-11-06 01:34:14 +0530

    product route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const {
-  authenticateUser,
-  authorizePermissions,
+    authenticateUser,
+    authorizePermissions,
 } = require('../middleware/authentication');
 
 const {
-  createProduct,
-  getAllProducts,
-  getSingleProduct,
-  updateProduct,
-  deleteProduct,
-  uploadImage,
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadImage,
 } = require('../controllers/productController');
 
 const { getSingleProductReviews } = require('../controllers/reviewController');
 
 router
-  .route('/')
-  .post([authenticateUser, authorizePermissions('admin')], createProduct)
-  .get(getAllProducts);
+    .route('/')
+    .post([authenticateUser], createProduct)
+    .get(getAllProducts);
 
 router
-  .route('/uploadImage')
-  .post([authenticateUser, authorizePermissions('admin')], uploadImage);
+    .route('/uploadImage')
+    .post([authenticateUser], uploadImage);
 
 router
-  .route('/:id')
-  .get(getSingleProduct)
-  .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
-  .delete([authenticateUser, authorizePermissions('admin')], deleteProduct);
+    .route('/:id')
+    .get(getSingleProduct)
+    .patch([authenticateUser], updateProduct)
+    .delete([authenticateUser], deleteProduct);
 
 router.route('/:id/reviews').get(getSingleProductReviews);
 
